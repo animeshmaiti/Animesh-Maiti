9f6fa9945d70538369c6f3690123fb4fdac7da7d
Animesh Maiti <a.maiti770@gmail.com>
2024-08-19 09:56:13 +0530

    main content- article,about-text,service

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,17 @@
 import { Navbar } from './Navbar'
-
+import {About} from './Pages/About'
+// import {Resume} from './Pages/Resume'
+// import {Project} from './Pages/Project'
+// import {Contact} from './Pages/Contact'
 
 export const Home = () => {
   return (
     <div className='main-content'>
       <Navbar/>
+      <About/>
+      {/* <Resume/>
+      <Project/>
+      <Contact/> */}
     </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,37 @@
-
-
 export const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav className='navbar'>
+      <ul className='navbar-list'>
+        <li className='navbar-item'>
+          <button className='navbar-link active' data-nav-link>
+            About
+          </button>
+        </li>
+
+        <li className='navbar-item'>
+          <button className='navbar-link' data-nav-link>
+            Resume
+          </button>
+        </li>
+
+        <li className='navbar-item'>
+          <button className='navbar-link' data-nav-link>
+            Portfolio
+          </button>
+        </li>
+
+        <li className='navbar-item'>
+          <button className='navbar-link' data-nav-link>
+            Blog
+          </button>
+        </li>
+
+        <li className='navbar-item'>
+          <button className='navbar-link' data-nav-link>
+            Contact
+          </button>
+        </li>
+      </ul>
+    </nav>
+  );
+};
