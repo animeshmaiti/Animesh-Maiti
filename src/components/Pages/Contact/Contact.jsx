904d979f9ad652834a7e3c987d88dceaf09235dc
Animesh Maiti <a.maiti770@gmail.com>
2024-10-26 01:33:56 +0530

    minor change

--- src/components/Pages/Contact/Contact.jsx
+++ src/components/Pages/Contact/Contact.jsx
@@ -61,43 +61,43 @@ export const Contact = () => {
   };
 
   return (
-    <article className="contact" data-page="contact">
+    <article className='contact' data-page='contact'>
       <header>
-        <h2 className="h2 article-title">Contact</h2>
+        <h2 className='h2 article-title'>Contact</h2>
       </header>
 
-      <section className="mapbox" data-mapbox>
+      <section className='mapbox' data-mapbox>
         <figure>
           <iframe
-            src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d7481.288041800725!2d85.81987715385534!3d20.356319542877074!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sin!4v1724477246818!5m2!1sen!2sin"
-            width="400"
-            height="300"
-            loading="lazy"
-            referrerPolicy="no-referrer-when-downgrade"
+            src='https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d7481.288041800725!2d85.81987715385534!3d20.356319542877074!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sin!4v1724477246818!5m2!1sen!2sin'
+            width='400'
+            height='300'
+            loading='lazy'
+            referrerPolicy='no-referrer-when-downgrade'
           ></iframe>
         </figure>
       </section>
 
-      <section className="contact-form">
-        <h3 className="h3 form-title">Contact Form</h3>
+      <section className='contact-form'>
+        <h3 className='h3 form-title'>Contact Form</h3>
 
-        <form className="form" onSubmit={handleSubmit}>
-          <div className="input-wrapper">
+        <form className='form' onSubmit={handleSubmit}>
+          <div className='input-wrapper'>
             <input
-              type="text"
-              name="fullName"
-              className="form-input"
-              placeholder="Full name"
+              type='text'
+              name='fullName'
+              className='form-input'
+              placeholder='Full name'
               required
               value={formData.fullName}
               onChange={handleChange}
             />
 
             <input
-              type="email"
-              name="email"
-              className="form-input"
-              placeholder="example@service.domain"
+              type='email'
+              name='email'
+              className='form-input'
+              placeholder='example@service.domain'
               required
               value={formData.email}
               onChange={handleChange}
@@ -105,15 +105,15 @@ export const Contact = () => {
           </div>
 
           <textarea
-            name="message"
-            className="form-input"
-            placeholder="Your Message"
+            name='message'
+            className='form-input'
+            placeholder='Your Message'
             required
             value={formData.message}
             onChange={handleChange}
           ></textarea>
 
-          <button className="form-btn" type="submit" disabled={!isFormValid}>
+          <button className='form-btn' type='submit' disabled={!isFormValid}>
             <IonIcon icon={paperPlaneOutline}></IonIcon>
             <span>Send Message</span>
           </button>
