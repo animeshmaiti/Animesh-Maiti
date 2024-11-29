657cef0987773a6ae4e0ccbdbe56125ae1ae9361
Animesh Maiti <a.maiti770@gmail.com>
2024-11-29 18:22:54 +0530

    rename file clients to tools

--- src/components/Pages/About/Tools.jsx
+++ src/components/Pages/About/Tools.jsx
@@ -1,6 +1,6 @@
 import { languageData } from '../../Data/Data';
 
-export const Clients = () => {
+export const Tools = () => {
   return (
     <section className='languages'>
       <h3 className='h3 languages-title'>Languages and Tools</h3>
@@ -8,7 +8,7 @@ export const Clients = () => {
       <ul className='languages-list'>
         {languageData.map((clientData, index) => (
           <li key={index} className='languages-item'>
-            <img src={clientData} alt='client logo' />
+            <img src={clientData} alt='logo' />
           </li>
         ))}
       </ul>
